8cddd6a7773977589c77fd517f9b41c62fb028a7
Nishith <IT18140576@my.sliit.lk>
2024-04-27 11:43:23 +0530

    wip: proj request validators

--- src/routes/v1_0/project.route.ts
+++ src/routes/v1_0/project.route.ts
@@ -1,27 +1,66 @@
 import express from 'express';
 
 import * as projectController from '../../controllers/project.controller';
+import * as projectValidator from '../../validators/project.validator';
 import * as authMiddleware from '../../middlewares/auth';
+import validationMiddleware from '../../middlewares/validate';
 
 const router = express.Router();
 
 router.get('/', authMiddleware.authRequired, projectController.getProjects);
-router.post('/', authMiddleware.authRequired, projectController.createProject);
 
-router.get('/:projectId', authMiddleware.authRequired, projectController.getProject);
-router.patch('/:projectId', authMiddleware.authRequired, projectController.updateProject);
-router.delete('/:projectId', authMiddleware.authRequired, projectController.deleteProject);
+router.post(
+  '/',
+  validationMiddleware(projectValidator.createProjectSchema),
+  authMiddleware.authRequired,
+  projectController.createProject
+);
 
-router.post('/:projectId/assign', authMiddleware.authRequired, projectController.addUser);
-router.delete('/:projectId/unassign', authMiddleware.authRequired, projectController.removeUser);
+router.get(
+  '/:projectId',
+  validationMiddleware(projectValidator.getProjectSchema),
+  authMiddleware.authRequired,
+  projectController.getProject
+);
+
+router.patch(
+  '/:projectId',
+  validationMiddleware(projectValidator.updateProjectSchema),
+  authMiddleware.authRequired,
+  projectController.updateProject
+);
+
+router.delete(
+  '/:projectId',
+  validationMiddleware(projectValidator.deleteProjectSchema),
+  authMiddleware.authRequired,
+  projectController.deleteProject
+);
+
+router.post(
+  '/:projectId/assign',
+  validationMiddleware(projectValidator.addOrRemoveUserSchema),
+  authMiddleware.authRequired,
+  projectController.addUser
+);
+
+router.delete(
+  '/:projectId/unassign',
+  validationMiddleware(projectValidator.addOrRemoveUserSchema),
+  authMiddleware.authRequired,
+  projectController.removeUser
+);
 
 router.get(
   '/:userId/created-projects',
+  validationMiddleware(projectValidator.getUserProjectsSchema),
   authMiddleware.authRequired,
   projectController.getCreatedProjectsByUser
 );
+
 router.get(
   '/:userId/assigned-projects',
+  validationMiddleware(projectValidator.getUserProjectsSchema),
   authMiddleware.authRequired,
   projectController.getAssignedProjectsByUser
 );
